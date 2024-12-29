1462d04f031985253f791cf2563cc44032d35989
SERGEY <xcreativ@ya.ru>
2024-12-29 13:42:21 +0300

    Migration code for houses table

--- database/migrations/2024_12_29_103819_create_houses_table.php
+++ database/migrations/2024_12_29_103819_create_houses_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('houses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            // Since the task does not specify exactly the data type of the columns below and whether it can be null,
+            // we are working with the possibility of empty values to avoid problems.
+            $table->unsignedInteger('price')->nullable();
+            // As a rule, the number of bathrooms and garages below is not more than 127.
+            // Therefore, in order to optimize the data, we choose tinyint
+            $table->unsignedTinyInteger('bedrooms')->nullable();
+            $table->unsignedTinyInteger('bathrooms')->nullable();
+            $table->unsignedTinyInteger('storeys')->nullable();
+            $table->unsignedTinyInteger('garages')->nullable();
             $table->timestamps();
         });
     }
